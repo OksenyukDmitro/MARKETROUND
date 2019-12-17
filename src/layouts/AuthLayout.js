5ab9dd36ea7da3243c8f2e62de732f18da83bd27
Oksenyuk Dmytro <Oksenyuk.Dmitro@ukr.net>
2019-12-17 18:12:44 +0200

    feat: added guest mode

--- src/layouts/AuthLayout.js
+++ src/layouts/AuthLayout.js
@@ -6,34 +6,58 @@ import {
   Nav,
   Container,
   NavItem,
-  NavLink,
+  Button,
+  Media,
 } from 'reactstrap';
 import logo from '../images/onlyLogo.png';
+import { Link } from 'react-router-dom';
+import routes from '../router/routes';
 
-const AuthLayout = ({ children }) => (
-  <>
-    <Navbar className="nav-bar " color="white" light expand="md">
-      <NavbarBrand href="#">
-        <img src={logo} alt="j" style={{ width: '60px' }} />
-      </NavbarBrand>
-      <Collapse navbar>
-        <Nav className="ml-auto" navbar>
-          <NavItem>
-            <NavLink href="#">
-                About us
-            </NavLink>
-          </NavItem>
-          <NavItem>
-            <NavLink href="#">
-                Why NAF?
-            </NavLink>
-          </NavItem>
-        </Nav>
-      </Collapse>
-    </Navbar>
-    <Container>
-      {children}
-    </Container>
-  </>
-);
+const AuthLayout = ({ children }) => {
+  return (
+    <>
+      <Navbar className="nav-bar " color="white" light expand="md">
+        <NavbarBrand >
+          <Link
+            to={routes.home}
+            className="mt-2 ml-2"
+          >
+            <Button
+              className="bg-transparent border-0"
+              style={{
+                margin: '0px',
+                marginLeft: '-12px',
+                marginTop: '-5px',
+                width: ' 62px',
+                height: ' 58px',
+              }}
+            >
+              <Media
+                src={logo}
+                alt="pic"
+                style={{
+                  width: ' 62px',
+                  marginLeft: '-20px',
+                  marginTop: '-8px',
+                }}
+              />
+            </Button>
+          </Link>
+        </NavbarBrand>
+        <Collapse navbar>
+          <Nav className="ml-auto" navbar>
+            <NavItem>
+              <Link to={routes.login}>
+                <Button>Login</Button>
+              </Link>
+            </NavItem>
+          </Nav>
+        </Collapse>
+      </Navbar>
+      <Container>
+        {children}
+      </Container>
+    </>
+  )
+};
 export default AuthLayout;
--- src/router/AuthRouter.js
+++ src/router/AuthRouter.js
@@ -6,6 +6,7 @@ import {
 } from 'react-router-dom';
 import LoginFormPage from '../pages/LoginFormPage';
 import CreateAccountFormPage from '../pages/CreateAccountFormPage';
+import ProductsPage from '../pages/ProductsPage';
 import AuthLayout from '../layouts/AuthLayout';
 import routes from './routes';
 
@@ -22,7 +23,11 @@ const AuthRouter = (p) => (
           <CreateAccountFormPage {...props} />
         )}
       />
-
+      <Route
+        exact
+        path={routes.home}
+        render={(props) => <ProductsPage {...props} />}
+      />
       <Route render={() => <Redirect to={routes.login} exact />} />
     </Switch>
   </AuthLayout>
